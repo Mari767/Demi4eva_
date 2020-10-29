bc3e20d1094ee7a97f8a2408a2f8ea3c3c06730b
Mari <mdemiceva3@gmail.com>
2020-10-29 13:59:49 +0200

    3

---
+++ Lab 1/Project 3/Project 3.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int a, b;
+
+	printf("Введите число a, что бы вознести его в восьмую степень.\n a=");
+	scanf_s("%d", &a);
+
+	b = a * a;
+	b = b * b;
+	b = b * b;
+
+	printf("%d в восьмой степени равно %d", a, b);
+
+	return 0;
+}
