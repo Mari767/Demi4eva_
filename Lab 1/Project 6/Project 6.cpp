e5d4eb4c4df70a209435f893d6050d1d234e98b7
Mari <mdemiceva3@gmail.com>
2020-10-29 14:07:17 +0200

    6

---
+++ Lab 1/Project 6/Project 6.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	double a, b, c;
+
+
+	printf("Эта программа считает, сколько секунд в текущем году. \nВведите количество лет для расчета. \n ");
+	scanf_s("%lf", &a);
+	c = 60 * 60 * 24 * 365;
+	b = c * a;
+	printf("В %.lf лет %.lf секунд.\n", a, b);
+
+}
