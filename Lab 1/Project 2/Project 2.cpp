19afb2d66a6f57726b13ccbd80a2f5de5a317f26
Mari <mdemiceva3@gmail.com>
2020-10-29 13:56:44 +0200

    2

---
+++ Lab 1/Project 2/Project 2.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    double a, b, c, p, S;
+    printf("Введите стороны a, b,c.\n а=");
+    scanf_s("%lf", &a);
+    printf(" b=");
+    scanf_s("%lf", &b);
+    printf(" c=");
+    scanf_s("%lf", &c);
+
+    p = (a + b + c) / 2;
+    printf("p=%lf \n", p);
+
+    S = sqrt(p * (p - a) * (p - b) * (p - c));
+    printf("S=%lf", S);
+}
