5e7592a4ee79378c184c565f5f228f752081bd2b
Mari <mdemiceva3@gmail.com>
2020-10-29 14:02:13 +0200

    4

---
+++ Lab 1/Project 4/Project 4.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	int a, b, c;
+	printf("Способ обмена местами значения 2-х переменных через дополнительную переменную \n");
+	printf("Введите значения а и b\n");
+
+	printf("\na=");
+	scanf_s("%d", &a);
+	printf("b=");
+	scanf_s("%d", &b);
+
+	c = a;
+	a = b;
+	b = c;
+	printf("\na=%d\nb=%d\n", a, b);
+
+	printf("\n Способ обмена местами значения 2-х переменных не используя дополнительной переменной\n");
+
+	b = a + b;
+	a = b - a;
+	b = b - a;
+	printf("\na=%d \nb=%d\n", a, b);
+}
