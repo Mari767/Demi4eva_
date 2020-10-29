2f84b4edeae56fa0654fd7bce766fb3f83afc480
Mari <mdemiceva3@gmail.com>
2020-10-29 13:52:29 +0200

    1

---
+++ Lab 1/Project 1/Project 1.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    printf("Тип данных int занимает %d байта. \n", sizeof(int));
+    printf("Тип данных short занимает %d байта. \n", sizeof(short));
+    printf("Тип данных long занимает %d байта. \n", sizeof(long));
+    printf("Тип данных char занимает %d байт. \n", sizeof(char));
+    printf("Тип данных float занимает %d байта. \n", sizeof(float));
+    printf("Тип данных double занимает %d байтов. \n", sizeof(double));
+
+    return 0;
+}
