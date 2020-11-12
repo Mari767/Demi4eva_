0e18675c3f844ff948d4c353f211794e8365f185
Mari <mdemiceva3@gmail.com>
2020-11-12 11:40:12 +0200

    2

---
+++ Lab 5/Project 2/Project 2.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <stdio.h>
+// найти корни квадратного уравнения
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	double a, b, c, x, y, D, a2, x1, x2;
+	printf("Введите a, b и c  \na=");
+	scanf_s("%lf", &a);
+	printf("b=");
+	scanf_s("%lf", &b);
+	printf("c=");
+	scanf_s("%lf\n", &c);
+	
+
+	//a = 3, b = -5, c = -7.4;
+	// формулы решения
+	a2 = 2 * a;
+	D = sqrt(b * b - 4 * a * c);
+	if (D < 0){
+	     printf("x не существует");
+	}
+	else {
+		x1 = (-b - D) / a2;
+		x2 = (-b + D) / a2;
+		printf("x1=%.2lf \nx2=%.2lf\n", x1, x2);
+	}
+	
+}
+
