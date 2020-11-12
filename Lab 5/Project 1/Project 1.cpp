8d5cf4c4bdb58653a93cc2b3a0c0b0567741ecea
Mari <mdemiceva3@gmail.com>
2020-11-12 11:37:47 +0200

    1

---
+++ Lab 5/Project 1/Project 1.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <stdio.h>
+int main()
+{//  Попадание точки в область    Вариант 7
+	setlocale(LC_ALL, "Russian");
+	
+	double x, y;
+	printf("\nВведите координаты точки   \nx=");
+	scanf_s("%lf", &x);
+	printf("y=");
+	scanf_s("%lf", &y);
+
+	//Проверка 
+	if ((x * x + y * y <= 1) || ((x <= 0 && x >= -1) && (y <= 0 && y >= -1)))
+	{
+		printf("Точка попадает в область\n");
+	}
+	else {
+		printf("Точка не попадает в область\n");
+	}
+
+	return main();
+}
+
