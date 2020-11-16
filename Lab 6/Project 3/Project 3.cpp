7ab3d4cc4d8ee1cb2c650d7ab3039e2ddf1812f1
Mari <mdemiceva3@gmail.com>
2020-11-16 13:30:34 +0200

    3

--- Lab 6/Project 3/Project 3.cpp
+++ Lab 6/Project 3/Project 3.cpp
@@ -8,7 +8,7 @@
 int main() {
 	setlocale(LC_ALL, "Russian");
 	int n, a, b, c, k;
-	int sum=0;
+	int sum=0, g=10;
 	srand(time(NULL));
 	printf("Проверь себя\n");
 
@@ -21,8 +21,8 @@ int main() {
 		if (c == k) {
 			++sum;
 		}
-		if (n == 10) {
-			printf("\nПравильных ответов %d из 10. Ваша оценка %d\n\n", sum, sum);
+		if (n == g) {
+			printf("\nПравильных ответов %d из %d. Ваша оценка %d\n\n", sum, g, sum);
 			break;
 		}
 	}
