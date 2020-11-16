b712f2742b0a029e5b288eeff67df41b6835f1f5
Mari <mdemiceva3@gmail.com>
2020-11-16 12:37:47 +0200

    2

---
+++ Lab 6/Project 2/Project 2.cpp
@@ -0,0 +1,36 @@
+﻿//Компьютер загадывает число. отгадать его (больше-меньше) и посчитать количество попыток.
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	srand(time(NULL));
+
+
+	int a, n, b;// human's number
+	b=rand() % 100;//computer's number
+
+	printf("Привет. Давай сыграем с тобой в игру.  Я загадал число от 0 до 100. Попробуй отгадать.\n");
+	
+	for (n = 1; ; n++) {
+		printf("\nВведи своё число ");
+		scanf_s("%d", &a);
+
+		if (a > b) {
+			printf("Твое число больше \n");
+		}
+		else if (a < b) {
+			printf("Твое число меньше\n");
+		}
+		else {
+			printf("Ты отгадал)\n");
+			printf("\nИспользовано %d попыток.\n\n", n);
+			break;
+		}
+
+
+	}
+	return 0;
+}
