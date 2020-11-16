aeb61003dcbc746cabd8a6b9c8368913283ecd49
Mari <mdemiceva3@gmail.com>
2020-11-16 12:40:15 +0200

    3

---
+++ Lab 6/Project 3/Project 3.cpp
@@ -0,0 +1,30 @@
+﻿//Проверка знаний таблицы умножения(10 примеров, случайным образом; выставляется оценка).
+#include <iostream>
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	int n, a, b, c, k;
+	int sum=0;
+	srand(time(NULL));
+	printf("Проверь себя\n");
+
+	for (n = 1; ; n++) {
+		a = rand() % 10+2;
+		b = rand() % 10+3;
+		k = a * b;
+		printf("\n%d) %d*%d=", n, a, b);
+		scanf_s("%d", &c);
+		if (c == k) {
+			++sum;
+		}
+		if (n == 10) {
+			printf("\nПравильных ответов %d из 10. Ваша оценка %d\n\n", sum, sum);
+			break;
+		}
+	}
+	return 0;
+}
