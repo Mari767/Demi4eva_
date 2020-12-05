363c696d4346d1fb26b4513241764c847837339e
Mari <mdemiceva3@gmail.com>
2020-12-06 01:08:50 +0200

    1

--- Lab 8/8 Б/8 Б.cpp
+++ Lab 8/8 Б/8 Б.cpp
@@ -5,63 +5,158 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#define N 10
+#define N 15
+#define M 50// rezerv
+int mask[M];
+int mas[N + M];
 
 int main() {
 	system("chcp 1251");
 	system("cls");
-	//srand(time(/*NULL*/));
+	srand(time(NULL));
 
-	int mas[N] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-	int a = 0;//индекс
+	int a;// ответ пользователя 
 	int k;//   кол-во переставных елементов
 	int k_;// позиция переставных елементов
+	int K;   //какой елемент добавляю
+	int u;// для массива Добавляемых елементов
 	int i;
-	for (i = 0; i < N; i++) {// Печать изначального масиива с резервом.
-		printf("%4d", mas[i]);
-	}
-	printf("\n\n\n\n\n");
 
-	//k = 1;      //        УДАЛЯЮ кол-во елементов           c 0 gjpbwbb
-	//k_ = 0;     //        c позиции 
-	//for (i = 0; i < N-1 ; i++) {
-	//	mas[i] = mas[i + 1];
-	//	printf("%4d", mas[i]);
+	for (i = 0; i < N; i++) {      // задаю массив
+		mas[i] = i + 1;
+	}
+	//for (i = 0; i < N; i++) {      // задаю массив РАНДОМАЙЗЕР
+	//	mas[i] = rand()%71-20;//-20  50
 	//}
-	printf("\n\n");
-
-	k = 1;      //        УДАЛЯЮ елемент
-	k_ = 5;     //        c позиции 
-	for (i = 0; i < N - 1; i++) {
+	printf("Первоначальный массив: ");
+	for (i = 0; i < N; i++) {//     Печать изначального масиива с резервом. ===========================
 		printf("%4d", mas[i]);
-		
-		/*if (i == k_) {
-			mas[i] = mas[i + 1];
-			printf("%4d", mas[i]);
-		}*/
-		/*else {
-			printf("%4d", mas[i]);
-		}*/
-
-
 	}
-	printf("\n\n\n\n\n");
-
+	printf("\n");
+	printf("\n\nИндекс элемента:       ");
 	
+	//Ввод данных
+	printf("Вы хотите удалить или добавить елементы массива\n");
+	printf("1) удалить\n");
+	printf("2) добавить\n");
+	scanf_s("%d", &a);
+	if (a != 1 && a != 2) {
+		printf("Вы некоректно ответили.\n");
+	}
 
+	//   Добавление и удаление
+	switch (a) {
+	case 1:
+		printf("Сколько елементов \nk=");  	//  Ввод данных
+		scanf_s("%d", &k);
+		if (k > N || k < 0) {
+			printf("Вы некоректно ответили.\n");
+			break;
+		}
+		else if (k == 0) {
+			printf("\nИзмененный массив: ");
+			for (i = 0; i < N; i++) {//     Печать изначального масиива
+				printf("%4d", mas[i]);
+			}
+			printf("\nИндексы элементов      ");
+			for (i = 1; i < N + 1; i++) {//     Печать индексов
+				printf("%4d", i);
+			}
+			break;
+		}
+		printf("С какой позиции ");
+		scanf_s("%d", &k_);
+		k_ = k_ - 1;
+		printf("\nПервоначальный массив: ");
+		for (i = 0; i < N; i++) {
+			printf("%4d", mas[i]);
+		}
+		printf("\nИндексы элементов      ");
+		for (i = 1; i < N + 1; i++) {//     Печать индексов
+			printf("%4d", i);
+		}
+		printf("\nИзмененный массив:     ");
+		for (i = 0; i < N - k; i++) {
+			if (i < k_) {
+				printf("%4d", mas[i]);
+			}
+			else {
+				mas[i] = mas[i + k];
+				printf("%4d", mas[i]);
+			}
+		}
+		printf("\nИндексы элементов      ");
+		for (i = 1; i < N + 1; i++) {//     Печать индексов
+			printf("%4d", i);
+		}
+		printf("\n\n");
+		break;
 
+	case 2:
+		printf("Сколько елементов \nk="); //    Ввод данных
+		scanf_s("%d", &k);
+		if (k > M || k < 0) {
+			printf("Вы не коректно ответили.\n");
+			break;
+		}
+		printf("На какую позицию ");
+		scanf_s("%d", &k_);
+		k_ = k_ - 1;
+		/*if (k = 0) {
+			printf("\nИзмененный массив:     ");
+			for (i = 0; i < N; i++) {
+				printf("%4d", mas[i]);
+			}
+			break;
+		}*/
+		if (k_ > N + 1 || k_ < 0) {
+			printf("Вы не коректно ответили.\n");
+			break;
+		}
 
+		printf("Введите элемент(ы) \n");
+		for (u = 0; u <= k - 1; u++) {// Заполнение массива, который содержит  добавляемые елементы
+			printf("%d) ", u + 1);
+			scanf_s("%d", &K);
+			mask[u] = K;
+		}
+		u = u - 1;//u= u+1 при выходе из цикла
 
+		printf("Первоначальный массив: ");
+		for (i = 0; i < N; i++) {//     Печать изначального масcива 
+			printf("%4d", mas[i]);
+		}
+		printf("\nИндексы элементов      ");
+		for (i = 1; i < N+1; i++) {//     Печать индексов
+			printf("%4d", i);
+		}
+		for (i = N + k - 1; i > -1; i--) {//  MAS[N+k]        добавляет и переворачивает  ++++++++++++++++++++++++
+			if (i > k_ + k - 1) {
+				mas[i] = mas[i - k];
+				//printf("%4d", mas[i]);
+			}
+			else if (i == k_ + k - 1) {
 
+				for (int j = 0; u >= 0; j++, u--) {
+					mas[i - j] = mask[u];
+					//printf("%4d", mas[i]);
+				}
+			}
+			else if (i < k_) {
+				//printf("%4d", mas[i]);
+			}
+		}
+		printf("\n\nИзмененный массив:     ");
 
-
-
-
-
-	printf("\n\n\n\n\n");
- }
-//printf("Первоначальный массив\n");
-	//for (int i = 0; i < A; i++) {
-	//	mas[i] = rand() % 101 - 45;// -20  20
-	//	printf("%5d",mas[i]);
-	//}
+		for (i = 0; i < N + k; i++) {//  Печать конечного масиива. ==========================
+			printf("%4d", mas[i]);
+		}
+		printf("\nИндексы элементов      ");
+			for (i = 1; i < N+k+1; i++) {//     Печать индексов
+				printf("%4d", i);
+		}
+		printf("\n\n");
+		break;
+	}
+	return 0;
+}
