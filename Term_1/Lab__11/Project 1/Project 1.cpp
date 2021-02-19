adc5542d1dbcc99fa9f282a1f437cfe4f8b41ea8
Mari <mdemiceva3@gmail.com>
2021-02-19 11:25:25 +0200

    )

---
+++ Term_1/Lab__11/Project 1/Project 1.cpp
@@ -0,0 +1,30 @@
+﻿// Вариант 7
+//Напишите рекурсивную функцию, которая вычисляет сумму цифр натурального числ# N.
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int Score(int N);
+
+int main() {
+	int N;
+	setlocale(LC_ALL, "ru");
+
+	cout << "Введите число N = ";
+	cin >> N;
+
+	cout << "Сумма цифр натурального числа N равна " << Score(N) << endl << endl;
+
+	return 0;
+}
+
+int Score(int N) {
+	if (N < 10) {
+		return N;
+	}
+	int a = N % 10;
+	N = N / 10;
+
+	return a + Score(N);
+}
