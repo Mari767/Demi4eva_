074429a262ff304647438c487010583619a79396
Mari <mdemiceva3@gmail.com>
2021-02-21 17:47:11 +0200

    $$

---
+++ Term_2/Lab 5/Main/Declaration.h
@@ -0,0 +1,16 @@
+#pragma once
+#ifndef DECLARATION_H_
+#define DECLARATION_H_
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+void Input_1(int* Pmas, int Size);
+
+int* Pmas;  /* указатель на начало массива */
+int Size;  /* размерность матрицы         */
+int* I;   /* текущий указатель у массиве */
+int i;   /* счетчик элементов*/
+
+#endif
