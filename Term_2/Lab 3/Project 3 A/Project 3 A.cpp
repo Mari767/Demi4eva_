cd3dc4569a6a01d787eb2ac1d7a21651b93da4f5
Mari <mdemiceva3@gmail.com>
2021-03-11 19:53:07 +0200

    )

---
+++ Term_2/Lab 3/Project 3 A/Project 3 A.cpp
@@ -0,0 +1,70 @@
+﻿//В одномерном массиве найти сумму и количество отрицательных элементов через :
+//а) относительный адрес;
+//б) через абсолютный адрес;
+//в) не использовать индексацию;
+//г) через массив указателей;
+//д) через указатель на указатель.
+//е) оформить в виде функции с параметрами указателями.
+
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "ru");
+	srand(time(NULL));
+	int Size, i;
+	int* ptr;
+	int Sum;
+	cout << "Введите размер массива." << endl;
+	cin >> Size;
+	int* mas = (int*)malloc(Size * sizeof(int));
+	for (i = 0; i < Size; i++) {
+		*(mas + i) = rand() % 200 - 70;
+	}
+	for (i = 0; i < Size; i++) {
+		cout << setw(4) << *(mas + i);
+	}
+
+	cout << " \nOтносительный адрес " << endl; 
+	
+	for (i = 0, Sum = 0; i < Size; i++) {
+		if (*(ptr + i) < 0) {
+			Sum += *(ptr + i);
+		}
+	}cout << "Sum = " << Sum << endl;
+
+	cout << "через абсолютный адрес" << endl;
+	//ptr = mas;
+	for (i = 0, Sum = 0; i < Size; i++, ptr++) {
+		if (*ptr < 0) {
+			Sum += *ptr;
+		}
+	}cout << "Sum = " << Sum << endl;
+
+	cout << "Не использовуя индексацию" << endl;
+	for (ptr=mas, Sum = 0; ptr < mas+ Size; ptr++) {
+		if (*ptr < 0) {
+			Sum += *ptr;
+		}
+	}cout << "Sum = " << Sum << endl;
+
+
+	free(mas);
+	return 0;
+}
+
+////Абсолютная адресация
+//p = A;
+//for (int i = 0; i < N; i++, p++)
+//    cout << *p << " ";
+//
+////Относительная адресация
+//p = A;
+//for (int i = 0; i < N; i++)
+//    cout << *(p + i) << " "; //*(A+i)
+//
+//  //Без индексов (без переменной цикла i)
+//for (p = A; p < A + N; p++)
+//    cout << *p << " ";
---
+++ Term_2/Lab 6/Project 1/Project 1.cpp
@@ -0,0 +1,15 @@
+﻿//Функция копирует строку в другую строку заданной длины и размещает текст первой строки по центру второй строки.
+
+#include <iostream>
+
+int main() {
+	setlocale(LC_ALL, "ru");
+	int Size;
+	scanf_s("%d", &Size);
+	char* str1 = (char*)malloc(sizeof(char) * Size);
+	char* str2 = (char*)malloc(sizeof(char) * Size);
+	if (Size < 25) {
+		printf("Строка слишком коротка. Для выполнения данной задачи выберете большую длинну строки\n");
+	}
+	printf("Введите строку\n");
+}
