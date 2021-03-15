33eaea44288541fbacdb64ee1c902780813486cc
Mari <mdemiceva3@gmail.com>
2021-03-15 23:15:58 +0200

    _)

--- Term_2/Lab 3/Project 3 A/Project 3 A.cpp
+++ Term_2/Lab 3/Project 3 A/Project 3 A.cpp
@@ -6,91 +6,82 @@
 //д) через указатель на указатель.
 //е) оформить в виде функции с параметрами указателями.
 
-
 #include <iostream>
 #include <iomanip>
-using namespace std;
 
 int main() {
 	setlocale(LC_ALL, "ru");
 	srand(time(NULL));
 	int Size, i;
 	int* ptr;
+	int** Pointer = NULL;
 	int Sum;
-	const int Size_ptr = 30;
-	int* Pointer[Size_ptr];
-	cout << "Введите размер массива. Максимальное количество -- 30 элементов." << endl;
-	cin >> Size;
-	int* mas = (int*)malloc(Size * sizeof(int));
+
+	printf("Введите размер массива.'\n");
+	scanf_s("%d", &Size);
+	int* mas = new int[Size];
+	int** Ptr_mas = new int* [Size];
+
+	//---------------------------------------------
+	//  Заполнение массива
 	for (i = 0; i < Size; i++) {
 		*(mas + i) = rand() % 200 - 70;
 	}
 	for (i = 0; i < Size; i++) {
-		cout << setw(4) << *(mas + i);
-	}
-	for (i = 0; i < Size; i++) {
-		Pointer = mas;
-	}
+		printf("%4d", *(mas + i));
+	}printf("\n\n");
 
+	//---------------------------------------------
 
-	//cout << " \nOтносительный адрес " << endl;
-	//ptr = mas;
-	//for (i = 0, Sum = 0; i < Size; i++) {
-	//	if (*(ptr + i) < 0) {
-	//		Sum += *(ptr + i);
-	//	}
-	//}cout << "Sum = " << Sum << endl;
-
-	//cout << "через абсолютный адрес" << endl;
-	////ptr = mas;
-	//for (i = 0, Sum = 0; i < Size; i++, ptr++) {
-	//	if (*ptr < 0) {
-	//		Sum += *ptr;
-	//	}
-	//}cout << "Sum = " << Sum << endl;
-
-	//cout << "Не использовуя индексацию" << endl;
-	//for (ptr = mas, Sum = 0; ptr < mas + Size; ptr++) {
-	//	if (*ptr < 0) {
-	//		Sum += *ptr;
-	//	}
-	//}cout << "Sum = " << Sum << endl;
-
-
-	cout << "Через массив указателей" << endl;
-
-	/*for (i = 0, Sum = 0; i < Size; i++) {
-		if (*(Pointer + i) < 0) {
-			Sum += *(Pointer + i);
+	printf("Oтносительный адрес.\n");
+	ptr = mas;
+	for (i = 0, Sum = 0; i < Size; i++) {
+		if (*(ptr + i) < 0) {
+			Sum += *(ptr + i);
 		}
-	}cout << "Sum = " << Sum << endl;*/
+	}printf("Sum = %d\n", Sum);
 
+	//---------------------------------------------
 
+	printf("Через абсолютный адрес.\n");
+	ptr = mas;
+	for (i = 0, Sum = 0; i < Size; i++, ptr++) {
+		if (*ptr < 0) {
+			Sum += *ptr;
+		}
+	}printf("Sum = %d\n", Sum);
 
-	//cout << "через указатель на указатель" << endl;
-	//ptr = mas;
-	//for (i = 0, Sum = 0; i < Size; i++/*, P_ptr++*/) {
-	//	if (**++P_ptr < 0) {
-	//		Sum += **P_ptr;
-	//	}
-	//}cout << "Sum = " << Sum << endl;
+	//---------------------------------------------
 
+	printf("Не использовуя индексацию.\n");
+	for (ptr = mas, Sum = 0; ptr < mas + Size; ptr++) {
+		if (*ptr < 0) {
+			Sum += *ptr;
+		}
+	}printf("Sum = %d\n", Sum);
 
+	//---------------------------------------------
 
-	free(mas);
+	printf("Через массив указателей.\n");
+	for (i = 0, Sum = 0; i < Size; i++) {
+		Ptr_mas[i] = &mas[i];
+		if (*Ptr_mas[i] < 0) {
+			Sum += *Ptr_mas[i];
+		}
+	}printf("Sum = %d\n", Sum);
+
+	//---------------------------------------------
+
+	printf("Через указатель на указатель.\n");
+	ptr = mas;
+	Pointer = &ptr;
+	for (i = 0, Sum = 0; i < Size; i++, (*Pointer)++) {
+		if (**Pointer < 0) {
+			Sum += **Pointer;
+		}
+	}printf("Sum = %d\n", Sum);
+
+	delete[] mas;
+	delete[] Ptr_mas;
 	return 0;
 }
-
-////Абсолютная адресация
-//p = A;
-//for (int i = 0; i < N; i++, p++)
-//    cout << *p << " ";
-//
-////Относительная адресация
-//p = A;
-//for (int i = 0; i < N; i++)
-//    cout << *(p + i) << " "; //*(A+i)
-//
-//  //Без индексов (без переменной цикла i)
-//for (p = A; p < A + N; p++)
-//    cout << *p << " ";
